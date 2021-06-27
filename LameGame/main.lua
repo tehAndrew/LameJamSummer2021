9a53d7f78f2e20b84b692ccdc14e7ec03306a1af
tehAndrew <andpa_kontakt@live.se>
2021-06-27 14:45:42 +0200

    Outlined what to do in main

--- LameGame/main.lua
+++ LameGame/main.lua
@@ -6,13 +6,10 @@ G_Debug = true
 -- Imports
 local Bullet = require("Bullet")
 local Fishmoose = require("Fishmoose")
-
-local enemyBulletAmount = 0
-local enemyBullets = {}
+local Boss = require("Boss")
 
 local fishmoose = {}
-
-local spawnTimer = 0
+local boss = {}
 
 -- Love callbacks
 function love.load()
@@ -23,50 +20,31 @@ function love.load()
     G_ResourceLoader.loadMeshResource("ShipMesh")
     G_ResourceLoader.loadShaderResource("MeshShader")
 
-    fishmoose = Fishmoose:init(love.graphics.getPixelWidth() / 2, love.graphics.getPixelHeight() / 2);
+    fishmoose = Fishmoose:init(love.graphics.getPixelWidth() / 2, love.graphics.getPixelHeight() - 100);
+    boss = Boss:init(love.graphics.getPixelWidth() / 2, 100);
+
+    --shipProjectiles = shipProjectiles:init()
+    --bossProjectiles = bossProjectiles:init()
+    --ship = ship:init(love.graphics.getPixelWidth() / 2, love.graphics.getPixelHeight() - 100, shipProjectiles);
+    --boss = Boss:init(love.graphics.getPixelWidth() / 2, 100, bossProjectiles);
 end
 
 function love.update(dt)
-    -- spawn
-    spawnTimer = spawnTimer + 1;
-    if spawnTimer >= 3 then
-        enemyBulletAmount = enemyBulletAmount + 1
-        enemyBullets[enemyBulletAmount] = Bullet:init(math.random(love.graphics.getPixelWidth()), 0, 80 + math.random() * 50, math.random(50) - 115, math.random(5, 6))
-        spawnTimer = 0
-    end
+    -- Check for potential collisions
+    --fishmoose:checkCollision(boss, bossBullets)
+    --boss:checkCollision(shipBullets)
 
-    -- update
     fishmoose:update(dt)
-
-    for i = 1, enemyBulletAmount do
-        enemyBullets[i]:update(dt)
-    end
-
-    -- fishmoose/bullet collision
-    for i = 1, enemyBulletAmount do
-        if fishmoose:checkCollision(enemyBullets[i]) then
-            enemyBullets[i].active = false;
-        end
-    end
-
-    -- remove inactive bullets
-    for i = enemyBulletAmount, 1, -1 do
-        if not enemyBullets[i].active then
-            table.remove(enemyBullets, i)
-            enemyBulletAmount = enemyBulletAmount - 1
-        end
-    end
-
-    
+    boss:update(dt)
+    --shipProjectiles:update(dt) -- also remove inactive bullets
+    --bossProjectiles:update(dt) -- also remove inactive bullets
 end
 
 function love.draw()
     fishmoose:draw()
-
-    for i = 1, enemyBulletAmount do
-        enemyBullets[i]:draw()
-    end
+    boss:draw()
+    --shipProjectiles:draw()
+    --bossProjectiles:draw()
 
     love.graphics.print("FPS: " .. love.timer.getFPS(), 10, 5);
-    love.graphics.print("Bullet #: " .. #enemyBullets, 10, 20);
 end
